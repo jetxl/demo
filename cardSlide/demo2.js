a12a3fab2fcf6f6a44dd5ab84d4ae163788f91fe
xieliang <mingyun.xl@163.com>
2016-09-28 15:04:49 +0800

    完成第二版的基本下拉动画

--- cardSlide/demo2.js
+++ cardSlide/demo2.js
@@ -3,8 +3,12 @@ $(function(){
 	$('.card').on('click',function(){
 		var card = $(this);
 		card.addClass('show1');
+		var time = 200;
 		setTimeout(function(){
 			card.addClass('show2');
-		},400);
+			setTimeout(function(){
+				card.addClass('show3');
+			},time);
+		},time);
 	})
 });
