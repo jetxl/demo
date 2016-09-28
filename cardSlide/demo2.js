436fd55f4b8241589485078ed2f0faf72f39babf
xieliang <mingyun.xl@163.com>
2016-09-28 16:48:51 +0800

    优化第二版细节

--- cardSlide/demo2.js
+++ cardSlide/demo2.js
@@ -1,14 +1,21 @@
 $(function(){
+	//more demo
+	$('.card-box').append(function(){
+		var cards = $(this).find('.card');
+		for(var i=10 ;i>0;i--){
+			$(this).append(cards.clone().addClass('style'+i%3).attr('style-type','style'+i%3));
+		}
+		return cards;
+	});
 
 	$('.card').on('click',function(){
 		var card = $(this);
-		card.addClass('show1');
-		var time = 200;
+		card.addClass('show1 active');
 		setTimeout(function(){
 			card.addClass('show2');
 			setTimeout(function(){
 				card.addClass('show3');
-			},time);
-		},time);
+			},200);
+		},300);
 	})
 });
